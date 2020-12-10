3c2a3c75a81e39f9516eae0e476913d89a2e8aa7
Olavo Alexandrino <oalexandrino@gmail.com>
2020-12-10 02:45:21 -0300

    refactoring

--- Template/task/creationdatelink.php
+++ Template/task/creationdatelink.php
@@ -5,6 +5,6 @@
             $project_id = $_GET["project_id"];
             $task_id = $_GET["task_id"];
         ?>
-        <?= $this->url->link(t('Update the created date for this task'), 'TaskCreatedDateController', 'creationdate', array('task_id' => $task_id ,'project_id' => $project_id ,'plugin' => 'TaskCreatedDate')) ?>
+        <?= $this->url->link(t('Update the creation date for this task'), 'TaskCreatedDateController', 'creationdate', array('task_id' => $task_id ,'project_id' => $project_id ,'plugin' => 'TaskCreatedDate')) ?>
     </p>
 <?php endif ?>
